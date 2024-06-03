cfec15b7854c4e8662a1202a31dc9ae6b926b43d
wendertech <wender.technos@gmail.com>
2024-06-03 17:11:37 -0300

    master

---
+++ JavaScript-Fundamentos/strings.js
@@ -0,0 +1,7 @@
+const escola = "Cod3r"
+
+console.log(escola.charAt(4)) // Returns the character at the specified index.
+console.log(escola.charAt(5))
+console.log(escola.charCodeAt(3)) //Returns the Unicode value of the character at the specified location.
+
+
